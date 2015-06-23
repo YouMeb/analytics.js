2c9ce69ef91cce45cbe0ab1c7f231135405bcb3d
Po-Ying Chen <poying.me@gmail.com>
2015-06-23 13:10:55 +0800

    test user

---
+++ test/user.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var User = require('../lib/user');
+
+describe('User', function () {
+  describe('with a function as siteUID', function () {
+    describe('#siteUID', function () {
+      it('should execute the function and get the result', function () {
+        var user = User({
+          siteUID: function () {
+            return 'test';
+          }
+        });
+        expect(user.siteUID()).to.equal('test');
+      });
+    });
+  });
+
+  describe('with siteUID', function () {
+    describe('#siteUID', function () {
+      it('should get the siteUID', function () {
+        var user = User({
+          siteUID: 'abc'
+        });
+        expect(user.siteUID()).to.equal('abc');
+      });
+    });
+  });
+});
